32759bd97f3c72ebaac4aac79bb79901c42ebf94
vaibhavc@latitudefintech.com <vaibhavc@latitudefintech.com>
2017-12-18 23:16:10 +0530

    bug fixes and features added

--- index.js
+++ index.js
@@ -1,40 +1,77 @@
 "use strict";
-var program = require('commander');
 const readline = require('readline');
 var Combinatorics = require('js-combinatorics');
-var replaceall = require("replaceall");
-//var Regex = require("regex");
 const fs = require('fs');
-const INPUT_DATA = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var Spinner = require('cli-spinner').Spinner;
+ //Spinner.setDefaultSpinnerString(15);
+const INPUT_DATA = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 //, "A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-const URL = "http://ekstep.in/co/"; // url to be concatenated
+const URL = "https://diksha.gov.in/tn/"; // url to be concatenated
 const INPUT_SEQUENCE_LENGTH = 6; // length of the sequence 
-const PUBLISHER_ID = "ABCD";
-const INPUT_PATTERN1 =  new RegExp(/^[0-9]+(,)+([0-9]+)?$/); //to Check two comma seprated numbers
-const INPUT_PATTERN2= new RegExp(/^[0-9]+([0-9]+)?$/); // to check only 1 number
-//const regex = new Regex(INPUT_PATTERN);
+const PUBLISHER_ID = "MHSC"; // PUBLISHER ID
+const SEPARATOR = ":"; // Separator between publisher id and sequence
+const FLAG = true; //true if want to add  publihser ID and sequence after comma
+const INPUT_PATTERN1 = new RegExp(/^[0-9]+(,)+([0-9]+)?$/); //to Check two comma seprated numbers
+const INPUT_PATTERN2 = new RegExp(/^[0-9]+([0-9]+)?$/); // to check only 1 number
+const MAX_LIMIT = 1000000; //Maximum number of sequences that can be genreated at one time max is 1000000
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-
-var finaloutput = [];
-
-
 var inputs = [];
 var start_index;
 var end_index;
-var cmb, a;
+var spinner;
 
 let writeStream = fs.createWriteStream('output.csv');
 
 writeStream.on('error', (e) => {
-  console.log('Program Exited with error code:',e.errno);
-   console.log('PLEASE CLOSE OUTPUT.CSV FILE IF ITS ALREADY OPEN');
+  console.log('Program Exited with error code:', e.errno);
+  console.log('PLEASE CLOSE OUTPUT.CSV FILE IF ITS ALREADY OPEN');
   process.exit();
 });
+
+
+/**
+* to validate the inputs entered by the user.
+* @param {string} answer 
+* @author Vaibhav Chaudhary
+*/
+
+function validateInputs(answer) {
+  try {
+
+    if (INPUT_PATTERN1.test(answer)) {
+
+      inputs = answer.split(',');
+      start_index = inputs[0] - 1;
+      end_index = inputs[1];
+      if (end_index < start_index) {
+        tryAgain();
+      }
+      rl.close();
+    }
+    else if (INPUT_PATTERN2.test(answer)) {
+
+      start_index = answer;
+      rl.close();
+
+    }
+    else {
+      tryAgain();
+
+    }
+  }
+  catch (e) {
+    console.log("Exception occured:", e);
+    tryAgain();
+
+  }
+
+
+}
 /**
 * to Get the Input from the user.
 *
@@ -44,36 +81,10 @@ writeStream.on('error', (e) => {
 getInput();
 
 function getInput() {
+  spinner = new Spinner({text:'Processing....'});
+ 
   rl.question('Enter the start and end position eg: 100,200 or only start position: ', (answer) => {
-    try {
-     // console.log("value entered by user",answer,INPUT_PATTERN1.test(answer));
-      if(INPUT_PATTERN1.test(answer)){
-     // console.log("coming insde fst if");
-      inputs = answer.split(',');
-      start_index = inputs[0] - 1;
-      end_index = inputs[1];
-      if(end_index<start_index){
-        tryAgain();
-      }
-      rl.close();
-      }
-    else if(INPUT_PATTERN2.test(answer)){
-     //  console.log("coming insde 2nd if");
-       start_index = answer;
-      rl.close();
-
-    }
-    else{
-      tryAgain();
-
-    }
-    }
-    catch (e) {
-      console.log("Exception occured:", e);
-      tryAgain();
-
-    }
-
+    validateInputs(answer);
   });
 
 }
@@ -86,7 +97,7 @@ function getInput() {
 
 function tryAgain() {
   rl.question('Entered inputs are invalid!! \nPress anything to exit or 1 to enter the inputs again: ', (answer) => {
-   //  console.log("value entered by user in try again",answer);
+    //  console.log("value entered by user in try again",answer);
     if (answer == 1) {
       getInput();
     }
@@ -99,13 +110,17 @@ function tryAgain() {
 
 
 rl.on('close', () => {
-  //console.log("Done taking inputs");
+  
+  spinner.start();
+  generateCombinations(INPUT_DATA, INPUT_SEQUENCE_LENGTH).then((e) => {
 
-   generateCombinations(INPUT_DATA,INPUT_SEQUENCE_LENGTH).then((e) => {
-    //console.log("Combinations has been created successfully");
+    //console.log(e);
+    writeStream.end();
+   // spinner.stop();
+  }).catch((e) => {
     console.log(e);
-   // writeStream.end();
-
+    writeStream.end();
+   // spinner.stop();
   })
 })
 
@@ -115,86 +130,59 @@ rl.on('close', () => {
 * @param {string} SEQUENCE_LENGTH 
 * @author Vaibhav Chaudhary
 */
-function generateCombinations(DATA,SEQUENCE_LENGTH) {
+function generateCombinations(DATA, SEQUENCE_LENGTH) {
   return new Promise((resolve, reject) => {
-   var count=0;
-    cmb = Combinatorics.baseN(DATA, SEQUENCE_LENGTH).filter((elem)=>{
-     // console.log(count,elem);
-       if(end_index!= undefined){
-         // console.log("end index is definedd");
-         //console.log("outside",a.toString().replace(/,/g, ''));
-        if (count >= start_index && count < end_index) {
-        //  console.log("inside",count,elem.toString().replace(/,/g, ''));
-          writeStream.write(URL +PUBLISHER_ID+'-'+ elem.toString().replace(/,/g, '') + '\n');
+    var count = 0;
+    var cmb = Combinatorics.baseN(DATA, SEQUENCE_LENGTH);
+    var cmb_length = cmb.length;
+    //console.log("das", cmb_length);
+    if (start_index > cmb_length || end_index > cmb_length) {
+      reject("Last index is greater than the total number of sequences");
+      return next();
+    }
+    else {
+      cmb.filter((elem) => {
+        //var cmb_length = cmb.length();
+        if (count == MAX_LIMIT + 1) {
+         // console.log("MAx limit reached");
+          resolve(MAX_LIMIT + " links has been generated successfully");
+          return next();
         }
-        else if(count > end_index){
-          console.log('dasad');
-         // process.exit();
-          resolve("added the number between index");
-          return next(); 
-        }
-        }
-      else
-        {
-         // console.log("no end index");
-           if (count >= start_index ) {
-          writeStream.write(URL +PUBLISHER_ID+'-'+ elem.toString().replace(/,/g, '') + '\n');
-        }
-        if (count==cmb_length-1){
-         // console.log("done");
-          resolve("All links have been generated successfully");
-          return;
-        }
-       
-        }
-        
-        // 
-        // count++;
-      
-      count++;
-    });
-    //var cmb_length = cmb.toArray().length;
-    //console.log("All arrays",cmb);
-    //console.log("spliced array");
-   // console.log(cmb.toArray().splice(start_index,end_index));
-    //resolve("All links have been generated successfully");
-  //  console.log("length if cmb",cmb_length);
-    //var count = 0;
-    // try {
-    //   while (a = cmb.next()) {
+        else {
+          if (end_index != undefined) {
 
-    //     if(end_index!= undefined){
-    //      // console.log("end index is definedd");
-    //      //console.log("outside",a.toString().replace(/,/g, ''));
-    //     if (count >= start_index && count < end_index) {
-    //      // console.log("inside",a.toString().replace(/,/g, ''));
-    //       writeStream.write(URL +PUBLISHER_ID+'-'+ a.toString().replace(/,/g, '') + '\n');
-    //     }
-    //     }
-    //   else
-    //     {
-    //      // console.log("no end index");
-    //        if (count >= start_index ) {
-    //       writeStream.write(URL +PUBLISHER_ID+'-'+ a.toString().replace(/,/g, '') + '\n');
-    //     }
-    //     }
-        
-    //     if (count==cmb_length-1){
-    //      // console.log("done");
-    //       resolve("All links have been generated successfully");
-    //     }
-    //     count++;
-    //   }
-    // }
-    // catch (e) {
-    //   console.log("Exception Occurred", e)
-    //   process.exit();
-    // }
+            if (count >= start_index && count < end_index) {
+              if(FLAG)
+              writeStream.write(URL + PUBLISHER_ID + SEPARATOR + elem.toString().replace(/,/g, '')+'\,'+PUBLISHER_ID + SEPARATOR + elem.toString().replace(/,/g, '') + '\n');
+              else
+                 writeStream.write(URL + PUBLISHER_ID + SEPARATOR + elem.toString().replace(/,/g, '') + '\n');
 
+            }
+            else if (count > end_index - 1) {
+              console.log('dasad');
+              resolve("All links have been generated successfully");
+              return next();
+            }
+          }
+          else {
+            if (count >= start_index) {
+              if(FLAG)
+              writeStream.write(URL + PUBLISHER_ID + SEPARATOR + elem.toString().replace(/,/g, '')+'\,'+PUBLISHER_ID + SEPARATOR + elem.toString().replace(/,/g, '')+ '\n');
+              else
+                writeStream.write(URL + PUBLISHER_ID + SEPARATOR + elem.toString().replace(/,/g, '') + '\n');
+
+            }
+          }
+        }
+        count++;
+      });
+    }
   })
 }
 
 
-// writeStream.on('end', () => {
-//   console.log('wrote all data to file');
-// });
+writeStream.on('finish', () => {
+  spinner.stop();
+  console.log('done');
+  
+});
